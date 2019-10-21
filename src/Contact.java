98cdcf151e6c8b21d9e8e238ebbaf3b8188cc306
gobto1833006 <gobto1833006@etu.cegepjonquiere.ca>
2019-10-21 10:58:47 -0400

    FINI

--- src/Contact.java
+++ src/Contact.java
@@ -1,25 +1,28 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Contact {
-    private String prenom="Tommy";
-    private  String nomDeFamille="Gobeil";
-    private String username="CarTon";
-    private String motDePasse="123";
-    private String cMotDePasse="123";
-    private String genre="Homme";
-    private String age="18";
-    private String condition="true";
-    HashMap<String,String> hashMap=new HashMap<>();
+    private String prenom = "";
+    private String nomDeFamille = "";
+    private String username = "";
+    private String motDePasse = "";
+    private String cMotDePasse = "";
+    private String genre = "";
+    private String age = "18";
+    private String condition = "false";
+    HashMap<String, String> hashMap = new HashMap<>();
+
 
     public Contact(/**String prenom, String nomDeFamille, String username, String motDePasse, int genre, int age*/) {
-        hashMap.put("Prénom",prenom);
-        hashMap.put("Nom de famille",nomDeFamille);
-        hashMap.put("Nom d'utilisateur",username);
-        hashMap.put("Mot de passe",motDePasse);
-        hashMap.put("Mot de passe ",cMotDePasse);
-        hashMap.put("Genre",genre);
-        hashMap.put("Âge",age);
-        hashMap.put("Condition",condition);
+        hashMap.put("Prénom", prenom);
+        hashMap.put("Nom de famille", nomDeFamille);
+        hashMap.put("Nom d'utilisateur", username);
+        hashMap.put("Mot de passe", motDePasse);
+        hashMap.put("Mot de passe ", cMotDePasse);
+        hashMap.put("Genre", genre);
+        hashMap.put("Âge", age);
+        hashMap.put("Condition", condition);
+
     }
 
 
@@ -27,7 +30,62 @@ public class Contact {
         return hashMap.get(key);
     }
 
-    public void setDonne(String key,String donne){
-        hashMap.replace(key,donne);
+    public void setDonne(String key, String donne) {
+        hashMap.replace(key, donne);
+    }
+
+    public String verification(ArrayList<String[]> tableauUsername) {
+        if (!(verificationToutRentré().equals(""))) {
+            return "Veuillez rentrer une information à la case: " + verificationToutRentré();
+        } else if (!(hashMap.get("Mot de passe").equals(hashMap.get("Mot de passe ")))) {
+            return "La confirmation du mot de passe n'est pas identique au mot de passe";
+        } else if (hashMap.get("Condition").equals("false")) {
+            return "Veuillez accepter les conditions d'utilisation";
+        } else if (usernamePris(hashMap.get("Nom d'utilisateur"), tableauUsername)) {
+            return "Ce nom d'utilisateur existe déjà";
+        } else return "";
+
+    }
+
+    private String verificationToutRentré() {
+        if (hashMap.get("Prénom").equals("")) {
+            return "Prénom";
+        } else if (hashMap.get("Nom de famille").equals("")) {
+            return "Nom de famille";
+        } else if (hashMap.get("Nom d'utilisateur").equals("")) {
+            return "Nom d'utilisateur";
+        } else if (hashMap.get("Mot de passe").equals("")) {
+            return "Mot de passe";
+        } else if (hashMap.get("Mot de passe ").equals("")) {
+            return "Confirmer le mot de passe";
+        } else if (hashMap.get("Genre").equals("")) {
+            return "Genre";
+        } else return "";
+    }
+
+    public void effacer() {
+        setDonne("Prénom", "");
+        setDonne("Nom de famille", "");
+        setDonne("Nom d'utilisateur", "");
+        setDonne("Mot de passe", "");
+        setDonne("Mot de passe ", "");
+        setDonne("Genre", "");
+        setDonne("Âge", "18");
+        setDonne("Condition", "false");
+    }
+
+    public String outputDonne() {
+        String output = getDonne("Prénom") + "," + getDonne("Nom de famille") + "," + getDonne("Nom d'utilisateur") + "," + getDonne("Mot de passe") + "," + getDonne("Mot de passe ") + "," + getDonne("Genre") + "," + getDonne("Âge") + "," + getDonne("Condition") + "\n";
+        return output;
+    }
+
+    public boolean usernamePris(String username, ArrayList<String[]> tableauContact) {
+        boolean bool = false;
+        for (int i = 0; i < tableauContact.size(); i++) {
+            if (username.equals(tableauContact.get(i)[0])) {
+                bool = true;
+            }
+        }
+        return bool;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,17 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -16,10 +27,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
         primaryStage.setTitle("Cube World definitive edition");
-        primaryStage.setFullScreen(true);
+        primaryStage.setMinHeight(920);
+        primaryStage.setMinWidth(920);
+        primaryStage.setFullScreen(false);
         Contact contact = new Contact();
 
+        File sauvegarde = new File("Sauvegarde.csv");
 
         Label nomUtilisateur = new Label("Nom d'utilisateur");
         TextField boiteNU = new TextField();
@@ -57,13 +72,18 @@ public class Main extends Application {
         homme.setToggleGroup(toggleGroup1);
         femme.setToggleGroup(toggleGroup1);
         autre.setToggleGroup(toggleGroup1);
-        homme.setSelected(true);
         Label age = new Label("Âge");
         Spinner boiteA = new Spinner(18, 150, 18);
         CheckBox conditionUtilisation = new CheckBox("J'accepte les conditions d'utilisations");
         Button inscription = new Button("S'inscrire");
         Button effacer = new Button("Effacer");
         Button retour = new Button("Retour");
+        Label messageErreur1 =new Label("");
+        Label messageErreur2 = new Label("");
+
+        ProgressIndicator progressIndicator=new ProgressIndicator();
+        Label chargement=new Label("Chargement du contenu");
+        chargement.setTranslateY(100);
 
 
         HBox hBox1 = new HBox(seConnecter, sInscrire);
@@ -75,16 +95,41 @@ public class Main extends Application {
         hBox2.setSpacing(5);
         HBox hBox3 = new HBox(inscription, effacer, retour);
         hBox3.setSpacing(5);
-        VBox vBox2 = new VBox(prenom, boiteP, nomDeFamille, boiteNDF, username, boiteU, password, boitePW, cPassword, boiteCPW, genre, hBox2, age, boiteA, conditionUtilisation, hBox3);
+        VBox vBox2 = new VBox(prenom, boiteP, nomDeFamille, boiteNDF, username, boiteU, password, boitePW, cPassword, boiteCPW, genre, hBox2, age, boiteA, conditionUtilisation, hBox3, messageErreur2);
         vBox2.setSpacing(10);
 
         Group group1 = new Group(vBox1);
-        group1.setTranslateX(800);
+        group1.setTranslateX(400);
         group1.setTranslateY(400);
         Group group2 = new Group(vBox2);
-        group2.setTranslateX(800);
+        group2.setTranslateX(400);
         group2.setTranslateY(200);
         Scene scene = new Scene(group1);
+        Group group3=new Group(progressIndicator,chargement);
+        group3.setTranslateX(400);
+        group3.setTranslateY(400);
+
+        seConnecter.setOnAction(event -> {
+            ArrayList<String[]> listeUsername = outUsername();
+            for (int i = 0; i < listeUsername.size(); i++) {
+                if (listeUsername.get(i)[0].equals(boiteNU.getText())) {
+                    MessageDigest digest = null;
+                    try {
+                        digest = MessageDigest.getInstance("SHA-256");
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
+                    byte[] encodedhash = digest.digest(
+                            boiteMDP.getText().getBytes(StandardCharsets.UTF_8));
+                    if (listeUsername.get(i)[1].equals(bytesToHex(encodedhash))) {
+                        scene.setRoot(group3);
+                    }
+
+                }else{
+                    messageErreur1.setText("La connexion a échouée");
+                }
+            }
+        });
 
 
         sInscrire.setOnAction(event -> {
@@ -103,30 +148,87 @@ public class Main extends Application {
             gestionBoite(boiteU, contact);
         }));
         boitePW.textProperty().addListener(((observable, oldValue, newValue) -> {
-            gestionMotDePasse(boitePW, contact);
+            try {
+                gestionMotDePasse(boitePW, contact);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
         }));
         boiteCPW.textProperty().addListener(((observable, oldValue, newValue) -> {
-            gestionMotDePasse(boiteCPW, contact);
+            try {
+                gestionMotDePasse(boiteCPW, contact);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
         }));
 
         toggleGroup1.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
 
-            if (toggleGroup1.getSelectedToggle()!=null){
+            if (toggleGroup1.getSelectedToggle() != null) {
 
-                contact.setDonne("Genre",toggleGroup1.getSelectedToggle().getUserData().toString());
-                System.out.println(contact.getDonne("Genre"));
+                contact.setDonne("Genre", toggleGroup1.getSelectedToggle().getUserData().toString());
+
             }
         });
         boiteA.valueProperty().addListener((observable, oldValue, newValue) -> {
-            contact.setDonne("Âge",Integer.toString((int)boiteA.getValue()));
-            System.out.println(contact.getDonne("Âge"));
+            contact.setDonne("Âge", Integer.toString((int) boiteA.getValue()));
+
         });
 
+        conditionUtilisation.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                contact.setDonne("Condition", "true");
+            } else {
+                contact.setDonne("Condition", "false");
 
+            }
+        });
 
 
         retour.setOnAction(event -> {
             scene.setRoot(group1);
+        });
+
+        inscription.setOnAction(event -> {
+
+            ArrayList<String[]> listeUsername = outUsername();
+            messageErreur2.setText(contact.verification(listeUsername));
+            if (contact.verification(listeUsername).equals("")) {
+                try {
+                    enregistrer(contact, sauvegarde);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                boiteP.setText("");
+                boiteNDF.setText("");
+                boiteU.setText("");
+                boitePW.setText("");
+                boiteCPW.setText("");
+                if (toggleGroup1.getSelectedToggle() != null) {
+                    toggleGroup1.getSelectedToggle().setSelected(false);
+                }
+                boiteA.decrement(Integer.parseInt(boiteA.getValue().toString()) - 18);
+                conditionUtilisation.setSelected(false);
+                messageErreur2.setText("");
+                contact.effacer();
+
+                scene.setRoot(group1);
+            }
+        });
+
+        effacer.setOnAction(event -> {
+            boiteP.setText("");
+            boiteNDF.setText("");
+            boiteU.setText("");
+            boitePW.setText("");
+            boiteCPW.setText("");
+            if (toggleGroup1.getSelectedToggle() != null) {
+                toggleGroup1.getSelectedToggle().setSelected(false);
+            }
+            boiteA.decrement(Integer.parseInt(boiteA.getValue().toString()) - 18);
+            conditionUtilisation.setSelected(false);
+            messageErreur2.setText("");
+            contact.effacer();
         });
 
 
@@ -139,11 +241,73 @@ public class Main extends Application {
     public void gestionBoite(TextField textField, Contact contact) {
 
         contact.setDonne(textField.getPromptText(), textField.getText());
-        System.out.println(textField.getPromptText() + " " + contact.getDonne(textField.getPromptText()));
+
     }
 
-    public void gestionMotDePasse(PasswordField passwordField, Contact contact) {
+    public void gestionMotDePasse(PasswordField passwordField, Contact contact) throws NoSuchAlgorithmException {
 
-        contact.setDonne(passwordField.getPromptText(), passwordField.getText());
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedhash = digest.digest(
+                passwordField.getText().getBytes(StandardCharsets.UTF_8));
+        contact.setDonne(passwordField.getPromptText(), bytesToHex(encodedhash));
+
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public static void enregistrer(Contact contact, File sauvegarde) throws IOException {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(sauvegarde, true));
+            writer.write(contact.outputDonne());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public static ArrayList<String[]> outUsername() {
+        String line;
+
+        ArrayList<String> tableauContact = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader("Sauvegarde.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                tableauContact.add(line);
+            }
+            bufferedReader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String[]> listeUsername = new ArrayList<>();
+        for (int i = 0; i < tableauContact.size(); i++) {
+            String[] strings = new String[2];
+            strings[0] = tableauContact.get(i).split(",")[2];
+            strings[1] = tableauContact.get(i).split(",")[3];
+            listeUsername.add(strings);
+        }
+        return listeUsername;
     }
 }
+
+
